e1622ddd198527ffcb065c987e737b63eb969640
FahimFaysalProgrammer <fahim.faysal.programmer@gmail.com>
2023-12-28 11:46:56 +0600

    final updated the task-project

--- task_project/task_project/urls.py
+++ task_project/task_project/urls.py
@@ -21,6 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='homepage'),
-    path('musician/', include('musician.urls')),
-    path('album/', include('album.urls')),
+    path('category/', include('category.urls')),
+    path('task/', include('task.urls')),
 ]
